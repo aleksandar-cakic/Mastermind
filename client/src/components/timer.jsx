0f702948e01f485b77da29eab1d4be103ca073d4
Aleksandar Cakic <acakic92@gmail.com>
2022-01-05 17:23:27 -0600

    Fix memory leak

--- client/src/components/timer.jsx
+++ client/src/components/timer.jsx
@@ -4,12 +4,17 @@ const Timer = ({updateScore}) => {
   let [timer, setTimer] = React.useState(1000);
 
   React.useEffect(() => {
-   timer > 0 && setTimeout(() => setTimer(timer - 1), 25)}, [timer]);
+   timer > 0 && setTimeout(() => setTimer(timer - 1), 1000)
+   return () => {
+    clearTimeout(timer);
+  }
+}, [timer]);
 
   React.useEffect(() => {
    if (timer === 0) {
+     // maybe start a new game or block further moves
      return updateScore('lost')
-   }}, [timer])
+   }}, [])
 
   return (
     <div className="timer">
