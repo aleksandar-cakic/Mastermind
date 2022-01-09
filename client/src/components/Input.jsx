b5ed8108b8a44952f3789678d0e6a965d75b97bc
Aleksandar Cakic <acakic92@gmail.com>
2022-01-09 17:16:14 -0600

    Update game logic, add highlights

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { StyledButton } from './Buttons/StyledButton.jsx';
 import '../stylesheets/input.css'
 
-const Input = ({ colors, handleColorClick }) => (
+const Input = ({ colors, handleColorClick, currentColor }) => (
   <div className='input'>
     {colors.map((color, i) => (
-      <div key={i} className='circle' onClick={handleColorClick} value={i} id={color}></div>
+      <div key={i} className={`circle ${color === currentColor ? 'border_checked' : 'border_unchecked'}`}  onClick={handleColorClick} value={i} id={color}></div>
     ))}
   </div>
 );
